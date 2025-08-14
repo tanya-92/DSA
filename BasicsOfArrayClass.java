0967e83eab1bdfb6606fb401e3bb2d4d439d58d6
tanya-92 <143723635+tanya-92@users.noreply.github.com>
2025-08-14 18:46:34 +0000

    intersection of two sorted arrays and missing number in array

--- BasicsOfArrayClass.java
+++ BasicsOfArrayClass.java
@@ -183,7 +183,7 @@ class BasicsOfArray{
         return -1;
     }
 
-    public void unionBrute(int[] arr1, int[] arr2){
+    public void unionOfSortedArraysBrute(int[] arr1, int[] arr2){
         int n1=arr1.length; int n2=arr2.length;
         Set<Integer> st = new TreeSet<>();  //ordered              :-for unordered(HashSet)    :-for Insertion order(LinkedHashSet)
         for(int i=0; i<n1; i++){
@@ -202,7 +202,7 @@ class BasicsOfArray{
         }
     }
 
-    public int[] unionOptimal(int[] arr1, int[] arr2){       //2 pointer approach
+    public int[] unionOfSortedArraysOptimal(int[] arr1, int[] arr2){       //2 pointer approach
         int i=0; int j=0;
         int n1=arr1.length; int n2=arr2.length;
         ArrayList<Integer> unionList = new ArrayList<>();
@@ -236,20 +236,95 @@ class BasicsOfArray{
         for (int k = 0; k < unionList.size(); k++) {
             result[k] = unionList.get(k);
         }
-        return result;
+        return unionList.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    public intersectionBrute(int[] arr1, int[] arr2){
+    public int[] intersectionOfSortedArraysBrute(int[] arr1, int[] arr2){
         int n1=arr1.length; int n2=arr2.length;
-        
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(arr1[i]==arr2[j]){
+                    ans.add(arr1[i]);
+                    break;
+                }
+                if(arr2[j]>arr1[i]) break;
+            }
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] intersectionOfSortedArraysOptimal(int[] arr1, int[] arr2){
+        int n1=arr1.length; int n2=arr2.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i=0;int j=0;
+        while(i<n1 && j<n2){
+            if(arr1[i]<arr2[j]){i++;}
+            else if(arr1[i]>arr2[j]){j++;}
+            else{
+                ans.add(arr1[i]);
+                i++; j++;
+            }
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public void missingNoBrute(int[] arr,int n){  
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }   
+        int i = 0;
+        for(i=1; i<=max; i++){
+            int flag = 0;
+            for(int j=0;j<n;j++){
+                if(arr[j]==i){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag==0) System.out.println(i);
+        }
+    }
+
+    public void missingNoBetter(int[] arr,int n){
+        int[] hash = new int[n+1];
+        for(int i=0; i<n; i++){
+            hash[arr[i]] = 1;
+        }
+        for(int i=1; i<=n; i++){
+            if(hash[i]==0) System.out.println(i);
+        }
+    }
+
+    public void missingNoOptimal1(int[] arr,int n){
+        int sum = (n*(n+1))/2;
+        int len = arr.length;
+        int sum2=0;
+        for(int i=0; i<len;i++){
+            sum2 = sum2 + arr[i];
+        }
+        System.out.println(sum-sum2);
+    }
+
+    public void missingNoOptimal2(int[] arr,int n){
+        int xor1 = 0; int xor2 = 0;
+        for(int i=0; i<n-1; i++){
+            xor2 = xor2 ^ arr[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        xor1 = xor1 ^ n;
+        System.out.println(xor1 ^ xor2);
     }
 }
 
 
 public class BasicsOfArrayClass {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,7,8,19};
-        int arr2[] = {2,4,5,9};
+        int arr[] = {1,2,4,5,6};
+        int arr2[] = {2,4,5,7,9};
         int n=arr.length;
         BasicsOfArray obj = new BasicsOfArray();
         // obj.secondSmallest(arr,n);
@@ -258,9 +333,17 @@ public class BasicsOfArrayClass {
         // obj.zerosToEndBrute(arr,n);
         // obj.display(arr,n);
         // obj.zerosToEndOptimal(arr,n);
-        obj.unionBrute(arr,arr2);
-        int[] ans = obj.unionOptimal(arr,arr2);
-        System.out.println(Arrays.toString(ans));
+        // obj.unionOfSortedArraysBrute(arr,arr2);
+        // int[] ans = obj.unionOfSortedArraysOptimal(arr,arr2);
+        // System.out.println(Arrays.toString(ans));
+        // int[] ans = obj.intersectionOfSortedArraysBrute(arr,arr2);
+        // System.out.println(Arrays.toString(ans));
+        // int[] ans = obj.intersectionOfSortedArraysOptimal(arr,arr2);
+        // System.out.println(Arrays.toString(ans));
+        // obj.missingNoBrute(arr,n);
+        // obj.missingNoBetter(arr,n+1);            //total length+missing one
+        // obj.missingNoOptimal1(arr,n+1);
+        obj.missingNoOptimal2(arr,n+1);
     }
 
 }

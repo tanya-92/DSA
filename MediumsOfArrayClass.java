f9e391d81dd8a6a6ea31e73df9a7567a9364435c
tanya-92 <143723635+tanya-92@users.noreply.github.com>
2025-09-25 19:59:09 +0000

    element occuring more than n/2(majority element)

--- MediumsOfArrayClass.java
+++ MediumsOfArrayClass.java
@@ -92,7 +92,34 @@ class MediumsOfArray{
         return -1;
     }
     public int majorityElementBetter(int[] arr, int n){
-        
+        Map<Integer, Integer> mpp = new HashMap<>();
+        for(int i=0; i<n; i++){
+            mpp.put(arr[i], getOrDefault(arr[i], 0)+1);
+        }
+        for(map.Entry<Integer, Integer> i : mpp.entrySet()){
+            if(i.getValue() > n/2){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+
+    public int majorityElementOptimal(int[] arr, int n){
+        int cnt = 0; int element = 0;
+        for(int i=0; i<n; i++){
+            if(cnt==0){
+                cnt=1;
+                element = arr[i];
+            }
+            else if(arr[i]==element) cnt++;
+            else cnt--;
+        }
+        int cnt1 = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==element) cnt1++;
+        }
+        if(cnt1 > n/2) return element;
+        return -1;
     }
 }
 

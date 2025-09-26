cfc9b0c236e1fa59c2ef84c5a6d015b995f208e1
tanya-92 <143723635+tanya-92@users.noreply.github.com>
2025-09-26 20:21:29 +0000

    Maximum Subarray Sum Solutions

--- MediumsOfArrayClass.java
+++ MediumsOfArrayClass.java
@@ -91,14 +91,15 @@ class MediumsOfArray{
         }
         return -1;
     }
+
     public int majorityElementBetter(int[] arr, int n){
         Map<Integer, Integer> mpp = new HashMap<>();
         for(int i=0; i<n; i++){
-            mpp.put(arr[i], getOrDefault(arr[i], 0)+1);
+            mpp.put(arr[i], mpp.getOrDefault(arr[i], 0)+1);
         }
-        for(map.Entry<Integer, Integer> i : mpp.entrySet()){
+        for(Map.Entry<Integer, Integer> i : mpp.entrySet()){
             if(i.getValue() > n/2){
-                return entry.getKey();
+                return i.getKey();
             }
         }
         return -1;
@@ -121,17 +122,97 @@ class MediumsOfArray{
         if(cnt1 > n/2) return element;
         return -1;
     }
+
+    public int maximumSubarraySumBruteBetter(int[] arr, int n){
+        int maximum = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            int sum = 0;
+            for(int j=i; j<n; j++){
+                sum += arr[j];
+                maximum = Math.max(sum,maximum);
+            }
+        }
+        return maximum;
+    }
+
+    public int maximumSubarraySumOptimal(int[] arr, int n){
+        int maximum = Integer.MIN_VALUE;
+        int sum = 0; int start = 0;
+        int ansStart = -1; int ansEnd = -1;
+        for(int i=0; i<n; i++){
+            if(sum==0) start = i;
+            sum += arr[i];
+            if(sum>maximum) {
+                maximum = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+            if(sum<0) sum = 0;
+        }
+        return maximum;
+    }
+
+    public void stocksProblem(int[] arr,int n){
+        int minimum = arr[0];
+        int profit = 0;
+        for(int i=0; i<n; i++){
+            int cost = arr[i] - minimum;
+            profit = Math.max(profit, cost);
+            minimum = Math.min(minimum,arr[i]);
+        }
+        System.out.println(profit);
+    }
+
+    public void reArrangeArrayElementBySignBrute(int[] arr, int n){
+        List<Integer> positives = new ArrayList<>();
+        List<Integer> negatives = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(arr[i]>0) positives.add(arr[i]);
+            else negatives.add(arr[i]);
+        }
+        for(int i=0; i<n/2; i++){
+            arr[2*i]=positives.get(i);
+            arr[2*i+1]=negatives.get(i);
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public int[] reArrangeArrayElementBySignOptimal(int[] arr, int n){
+        int[] ans = new int[n];
+        int positiveIndex = 0, negativeIndex = 1;
+        for(int i=0; i<n; i++){
+            if(arr[i]<0){
+                ans[negativeIndex] = arr[i];
+                negativeIndex+=2;
+            }
+            else{
+                ans[positiveIndex] = arr[i];
+                positiveIndex+=2;
+            }
+        } 
+        return ans;
+    }
 }
 
 public class MediumsOfArrayClass {
     public static void main(String[] args){
-        int arr[] = {0,0,1,2,0,2,2,2,1,0,1,0,0,1};
-        int arr2[] = {2,2,3,3,1,2,2};
+        int arr[] = {-2,-3,-4,-1,-2};
+        int arr2[] = {3,1,-2,-5,2,-4};
         int n=arr2.length;
         MediumsOfArray obj = new MediumsOfArray();
         // obj.twoSumOptimal(arr,n,14);
         // obj.sort0s1s2sBetter(arr,n);
         // obj.sort0s1s2sOptimal(arr,n);
-        System.out.println(obj.majorityElementBrute(arr2,n));
+        // System.out.println(obj.majorityElementBrute(arr2,n));
+        // System.out.println(obj.majorityElementBetter(arr2,n));
+        // System.out.println(obj.majorityElementOptimal(arr2,n));
+        // System.out.println(obj.maximumSubarraySumBruteBetter(arr,n));
+        // System.out.println(obj.maximumSubarraySumOptimal(arr,n));
+        // obj.stocksProblem(arr2,n);
+        // obj.reArrangeArrayElementBySignBrute(arr2,n);
+        int[] reArranged = obj.reArrangeArrayElementBySignOptimal(arr2,n);
+        System.out.println(Arrays.toString(reArranged));
     }
 }

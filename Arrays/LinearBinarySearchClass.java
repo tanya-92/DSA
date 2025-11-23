b61d14d11841ecdad2bfc7f055311003fee37911
Tanya Yadav <143723635+tanya-92@users.noreply.github.com>
2025-11-23 10:00:24 +0000

    Insertion and deltion on arrays

---
+++ Arrays/LinearBinarySearchClass.java
@@ -0,0 +1,21 @@
+class LinearBinarySearch{
+    public void display(int[] arr, int n){
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);    //traversal
+        }
+    }
+
+    
+}
+
+public class LinearBinarySearchClass{
+    public static void main(String[] args) {
+        int[] arr = new int[100];  // size = 100
+        arr[0] = 7;
+        arr[1] = 8;
+        arr[2] = 12;
+        arr[3] = 27;
+        arr[4] = 88;
+        
+    }
+}

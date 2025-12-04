b9bf64c402566d2378d7876315c3fb981137c272
Tanya Yadav <143723635+tanya-92@users.noreply.github.com>
2025-12-04 05:00:43 +0000

    completed with insertion in a linked list

--- LinkedLists/InsertionClass.java
+++ LinkedLists/InsertionClass.java
@@ -13,6 +13,31 @@ class Insertion{
 
     public void insertionAtBeginning(int data){
         Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void insertionAtIndex(int data, int index){
+        Node newNode = new Node(data);
+
+        // Case 1: insert at beginning (index = 0)
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        
+        Node temp = head; int i=0;
+        while(i != index-1){
+            temp = temp.next;        //index se ek step phle aake rokdia!
+            i++; 
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void insertionAtEnd(int data){
+        Node newNode = new Node(data);
         if(head==null){
             head = newNode;
             return;
@@ -22,6 +47,13 @@ class Insertion{
             temp = temp.next;
         }
         temp.next = newNode;
+        newNode.next = null;
+    }
+
+    public void insertionAfterNode(int data, Node prevNode){
+        Node newNode = new Node(data);
+        newNode.next = prevNode.next;  
+        prevNode.next = newNode;
     }
 
     public void traversal(){
@@ -38,10 +70,15 @@ class Insertion{
 public class InsertionClass{
     public static void main(String[] args){
         Insertion list = new Insertion();
-        list.insert(10);
-        list.insert(20);
-        list.insert(30);
-        list.insert(40);
+        list.insertionAtEnd(10);
+        list.insertionAtEnd(20);
+        list.insertionAtEnd(30);
+        list.insertionAtEnd(40);
+        list.insertionAtBeginning(50);
+        list.insertionAtIndex(60,2);
+        list.insertionAfterNode(50, head.next);          // insert after 2nd node
+        list.insertionAfterNode(50, head.next.next);     // insert after 3rd node
+
 
         System.out.println("Linked List:");
         list.traversal();

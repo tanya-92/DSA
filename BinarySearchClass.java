45a8bd252ed2626d4df9bd811832d181ce5fbb55
tanya-92 <143723635+tanya-92@users.noreply.github.com>
2026-01-23 19:54:51 +0000

    did binary search concepts: finding n-th root of m, minimum number of bananas kok eat in h hours, minimum days to make M bouquets

--- BinarySearchClass.java
+++ BinarySearchClass.java
@@ -197,18 +197,153 @@ class BinarySearch{
         }
         return index;
     }
+
+    public int NthRootOfMliearSearch(int n, int m){
+        for(int i=1; i<=m; i++){
+            if(FunctionNthRootOfMliearSearch(i,n) == m){         //function to multiply i, n times
+                return i;
+            }
+            else if(FunctionNthRootOfMliearSearch(i,n) > m) break;
+        }
+        return -1;
+    }
+    public int FunctionNthRootOfMliearSearch(int i, int n){
+        int result = 1;
+        for(int j=1; j<=n; j++){
+            result *= i;
+        }
+        return result;
+    }
+
+    public int NthRootOfMbinarySearch(int n, int m){
+        int low=1; int high=m;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(FunctionNthRootOfMbinarySearch(mid,n,m)==1){
+                return mid;
+            }
+            else if(FunctionNthRootOfMbinarySearch(mid,n,m)==0){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+    public int FunctionNthRootOfMbinarySearch(int mid, int n, int m){
+        long ans = 1;
+        for(int i=1; i<=n; i++){
+            ans = ans*mid;
+            if(ans>m) return 2;
+        }
+        if(ans == m) return 1;
+        return 0;
+    }
+
+    public int maxInArray(int[] arr){
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>maxi) maxi=arr[i];
+        }
+        return maxi;
+    }
+    public int minInArray(int[] arr){
+        int mini = Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<mini) mini=arr[i];
+        }
+        return mini;
+    }
+    public int kokoEatingBananasLinearSearch(int[] piles, int hours){           //time limit exceed
+        int max = maxInArray(piles);
+        for(int i=1; i<max; i++){
+            int requiredTime = FunctionKokoEatingBananas(piles,i);
+            if(requiredTime <= hours) return i;
+        }
+        return -1;
+    }
+    public int FunctionKokoEatingBananas(int[] piles, int hourly){
+        int totalHours = 0;
+        for(int i=0; i<piles.length; i++){
+            totalHours += Math.ceil((double)piles[i] / hourly);
+        }
+        return totalHours;
+    }
+
+    public int kokoEatingBananasBinarySearch(int[] piles, int hours){
+        int low = 1; int high = maxInArray(piles);
+        int ans = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int requiredTime = FunctionKokoEatingBananas(piles,mid);
+            if(requiredTime <= hours){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        } 
+        return ans;
+    }
+
+    public boolean FunctionMinimumDaysToMakeMbouquets(int[] bloomDays, int day, int m, int k){
+        int count = 0; int noOfBouquets = 0;
+        for(int i=0; i<bloomDays.length; i++){
+            if(bloomDays[i] <= day) count++;               //bloom hogya usdin ya usdin se phle
+            else{
+                noOfBouquets += (count/k);
+                count=0;
+            }
+        }
+        noOfBouquets += (count/k);
+        if(noOfBouquets >= m) return true;
+        else return false;
+    }
+    public int minimumDaysToMakeMbouquetsLinearSearch(int[] bloomDays, int m, int k){
+        int maxi = maxInArray(bloomDays);
+        int mini = minInArray(bloomDays);
+        if(bloomDays.length < m*k) return -1;
+        for(int i=mini; i<=maxi; i++){
+            if (FunctionMinimumDaysToMakeMbouquets(bloomDays,i,m,k)==true) return i;
+        }
+        return -1;
+    }
+
+    public int minimumDaysToMakeMbouquetsBinarySearch(int[] bloomDays, int m, int k){
+        int high = maxInArray(bloomDays);
+        int low = minInArray(bloomDays);
+        int ans = 0;
+        if(bloomDays.length < m*k) return -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(FunctionMinimumDaysToMakeMbouquets(bloomDays,mid,m,k)==true){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
 }
 
 public class BinarySearchClass {
     public static void main(String[] args){
         int[] arr = {2,4,6,8,8,8,11,13};
         int n=arr.length;
+        int[] piles = {3,6,11,7};
+        int[] bloomDays = {7,7,7,7,13,11,12,7};
         BinarySearch obj = new BinarySearch();
         // System.out.println(obj.search(arr,9));
         // System.out.println(obj.searchRecursive(arr,0,7,9));
         // System.out.println(obj.floor(arr,25));
-        int[] result = obj.firstAndLastOccurence(arr, 8);
-        System.out.println(Arrays.toString(result));
+        // int[] result = obj.firstAndLastOccurence(arr, 8);
+        // System.out.println(Arrays.toString(result));
+        // System.out.println(obj.NthRootOfMbinarySearch(3,64));
+        // System.out.println(obj.kokoEatingBananasLinearSearch(piles,8));
+        // System.out.println(obj.kokoEatingBananasBinarySearch(piles,8));
+        System.out.println(obj.minimumDaysToMakeMbouquetsBinarySearch(bloomDays,2,3));
 
     }
 }
